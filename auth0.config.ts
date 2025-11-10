c65411c323d7d7cfb0e6215a1f32d9a17f61a92d
Alan Asmis <asmisalan@gmail.com>
2025-11-10 01:21:26 -0300

    chore: config

--- auth0.config.ts
+++ auth0.config.ts
@@ -1,12 +1,13 @@
+// Testing credentials
 export const auth0Config = {
-  domain: 'dev-ksd1hbcay1mfvjjg.us.auth0.com',
-  clientId: 'NH5dRW1Gvcv0GeZDNzmNnsa0eCcp4799',
-  audience: 'https://nexosdi.synapxix.api',
-  scope: 'openid profile email',
+  domain: '',
+  clientId: '',
+  audience: '',
+  scope: '',
   api: {
-    baseUrl: 'http://localhost:3000/api',
+    baseUrl: '',
   },
-  issuer: 'https://dev-ksd1hbcay1mfvjjg.us.auth0.com/',
+  issuer: '',
 } as const;
 
 export type Auth0Config = typeof auth0Config;
