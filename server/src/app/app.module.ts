94a5dd112a291f90d93a3fb1205a0c9716e6472d
santiago <santisigot27@gmail.com>
2026-05-18 22:01:58 -0300

    feat: initialize AppModule with core modules and add validation and config dependencies

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -6,16 +6,17 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { LearningModule } from './learning/learning.module';
 import { ResearchModule } from './modules/research/research.module';
-import {ConfigModule} from "@nestjs/config";
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    AuthModule, 
-    LearningModule
-  , ResearchModule, ConfigModule.forRoot({ isGlobal: true }),],
+    AuthModule,
+    LearningModule,
+    ResearchModule,
+  ],
   controllers: [AppController],
   providers: [AppService, JwtAuthGuard],
 })
